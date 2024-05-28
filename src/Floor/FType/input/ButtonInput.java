f66e9d4953acdd43ba410bc5f4da490abebb6a09
xie100 <2948049467@qq.com>
2024-05-28 16:38:01 +0800

    1

--- src/Floor/FType/input/ButtonInput.java
+++ src/Floor/FType/input/ButtonInput.java
@@ -1,5 +1,7 @@
 package Floor.FType.input;
 
+import arc.Core;
+import arc.input.GestureDetector;
 import arc.math.geom.Vec2;
 import mindustry.input.InputHandler;
 
@@ -9,11 +11,14 @@ public class ButtonInput extends InputHandler {
     }
 
     @Override
-    public boolean locked() {
-        return true;
+    public void panCamera(Vec2 po) {
     }
 
     @Override
-    public void panCamera(Vec2 po) {
+    public void add(){
+        super.add();
+        Core.input.getInputProcessors().remove(i -> i instanceof InputHandler || (i instanceof GestureDetector && ((GestureDetector)i).getListener() instanceof InputHandler));
+        Core.input.addProcessor(detector = new GestureDetector(20, 0.5f, 0.3f, 0.15f, this));
+        Core.input.addProcessor(this);
     }
 }
