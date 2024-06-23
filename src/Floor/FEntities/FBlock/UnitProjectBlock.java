8b7d8b63a933bb80c41abb76d3b03a3c08bd46d5
xie100 <2948049467@qq.com>
2024-06-23 20:14:09 +0800

    写

--- src/Floor/FEntities/FBlock/UnitProjectBlock.java
+++ src/Floor/FEntities/FBlock/UnitProjectBlock.java
@@ -8,10 +8,14 @@ import arc.util.io.Writes;
 import mindustry.content.Fx;
 import mindustry.entities.Effect;
 import mindustry.entities.abilities.Ability;
+import mindustry.entities.effect.WaveEffect;
 import mindustry.entities.units.WeaponMount;
 import mindustry.game.Team;
 import mindustry.gen.Building;
 import mindustry.gen.Icon;
+import mindustry.gen.Unit;
+import mindustry.graphics.Drawf;
+import mindustry.graphics.Pal;
 import mindustry.world.Block;
 import mindustry.world.Tile;
 
@@ -20,7 +24,21 @@ import static mindustry.Vars.player;
 
 public class UnitProjectBlock extends Block {
     private static int num = 0;
+    public float pointReload = 60;
+    public float laserReload = 120;
     public Effect applyEffect = Fx.unitDespawn;
+    public Effect laserEffect = Fx.none;
+    public Effect pointEffect = new WaveEffect() {{
+        lifetime = 45;
+        sides = 4;
+        colorFrom = Pal.redLight;
+        lightColor = Pal.redLight;
+        colorTo = Pal.redLight;
+        sizeFrom = 10;
+        sizeTo = 30;
+        strokeFrom = 3;
+        strokeTo = 0;
+    }};
 
     public UnitProjectBlock(String name) {
         super(name);
@@ -33,17 +51,41 @@ public class UnitProjectBlock extends Block {
         return num == 0;
     }
 
+    @Override
+    public void drawPlace(int x, int y, int rotation, boolean valid) {
+        super.drawPlace(x, y, rotation, valid);
+
+        Unit u = player.unit();
+        Drawf.light(u.x, u.y, u.type.region, u.rotation, Pal.lightOrange, 12);
+    }
+
     public class UnitProjectBuild extends Building {
+        float pointTimer = 0;
+        float laserTimer = 0;
 
         @Override
         public void updateTile() {
+            pointTimer++;
+            laserTimer++;
             if (projects == null) {
                 ProjectsLocated.create();
             }
-            if (player.unit() != null && player.unit().spawnedByCore) {
-                if (!(player.unit().mounts[player.unit().mounts.length - 1] == sign)) {
-                    player.unit().apply(FStatusEffects.StrongStop, 180);
-                    projects.upper.get(player.unit());
+            Unit play = player.unit();
+            if (play != null && play.spawnedByCore) {
+                if (!(play.mounts[play.mounts.length - 1] == sign)) {
+                    play.apply(FStatusEffects.StrongStop, 180);
+                    applyEffect.at(player.unit().x, player.unit().y, 0, player.unit());
+                    projects.upper.get(play);
+                }
+            }
+            if (play != null) {
+                if (pointTimer >= pointReload) {
+                    pointTimer = 0;
+                    pointEffect.at(play.x, play.y, play.rotation, play);
+                }
+                if (laserTimer >= laserReload) {
+                    laserTimer = 0;
+                    laserEffect.at(play.x, play.y, play.rotation, new float[]{this.x, this.y, play.x, play.y});
                 }
             }
         }
@@ -78,7 +120,6 @@ public class UnitProjectBlock extends Block {
                 projects.setZero();
                 if (player.unit() != null && player.unit().spawnedByCore) {
                     projects.upper.get(player.unit());
-                    applyEffect.at(player.unit().x, player.unit().y, 0, player.unit());
                     player.unit().unapply(eff);
                     WeaponMount[] mount = new WeaponMount[player.unit().type.weapons.size];
                     System.arraycopy(player.unit().mounts, 0, mount, 0, mount.length);
